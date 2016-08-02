cd9739663659e7141f2798f816b3bc91b8298be0
GG7190 <greengirlsrobotics@gmail.com>
2016-08-02 18:40:06 -0500

    SF CM RL- We moved all the old code into the new structure.

---
+++ FtcRobotController/src/main/java/com/greengirlsold/Sensor.java
@@ -0,0 +1,7 @@
+package com.greengirlsold;
+
+/**
+ * Created by G201956 on 9/23/2015.
+ */
+public class Sensor {
+}
---
+++ FtcRobotController/src/main/java/com/greengirlsold/drivingMotor.java
@@ -0,0 +1,10 @@
+package com.greengirlsold;
+
+/**
+ * Created by G201956 on 9/20/2015.
+ */
+public class drivingMotor extends Motor{
+    public void goForward(int speed){
+
+    }
+}
